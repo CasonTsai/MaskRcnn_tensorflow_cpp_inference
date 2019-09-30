9f78267b0e046f785998245606661ad083ae747a
cason <jifengfuxiaocsz@163.com>
2019-09-30 23:01:04 +0800

    maskRcnn_tf_cpp_v0

---
+++ data_format.h
@@ -0,0 +1,41 @@
+#ifndef DATA_FORMAT_H
+#define DATA_FORMAT_H
+#include <vector>
+#include <map>
+#include <string>
+#include "tensorflow/core/framework/tensor.h"
+const size_t ColorTable[50]={0xBBFFFF,0x00868B,0x00FF00,0x008B45,
+                  0xBCEE68,0xFFF68F,0x8B864E,0xFFFFE0,
+                  0x8B8B7A,0xFFFF00,0x8B8B00,0x8B658B,
+                  0xFFC1C1,0x8B6969,0xFF6A6A,0x8B3A3A,
+                  0xFF8247,0xFF3030,0xFF69B4,0x0000FF,
+                  0x00688B
+
+                  };//颜色对照表
+
+
+struct boxForXml{
+    int y1,x1,y2,x2;
+    float scores=0.f;
+
+
+};
+struct boxInfo{
+    int y1,x1,y2,x2;
+    int classId=0;
+    float scores=0.f;
+    int boxNum=-1;
+};
+
+struct imageDetectInfo{
+    int imageWidth=0;//not yet
+    int imageHeight=0;//not yet
+    int imageNum=-1;
+    std::vector<boxInfo> detectInfo;
+
+};
+
+
+
+
+#endif // DATA_FORMAT_H
